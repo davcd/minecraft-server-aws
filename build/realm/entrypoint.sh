1ccf42e837690cfbf8eb4ba9caf644d7514fa19b
David Carvajal <dcarvajal@twilio.com>
2023-08-17 00:05:03 +0200

    chore: added docker compose server setup

---
+++ build/realm/entrypoint.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+ JAVA_OPTS="-server \
+            -XX:+UnlockExperimentalVMOptions \
+            -XX:+UseG1GC \
+            -XX:MaxGCPauseMillis=200 \
+            -XX:InitiatingHeapOccupancyPercent=70 \
+            -XX:-OmitStackTraceInFastThrow"
+
+ exec java $DEBUG_OPTS $JAVA_OPTS -jar /server/paper.jar
